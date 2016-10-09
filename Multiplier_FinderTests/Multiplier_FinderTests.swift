7838927ad8da5066d2da7cad56d6d803aa8b2559
SaundersB <speakeasys@gmail.com>
2016-10-08 18:35:12 -0700

    Initial Commit

---
+++ Multiplier_FinderTests/Multiplier_FinderTests.swift
@@ -0,0 +1,36 @@
+//
+//  Multiplier_FinderTests.swift
+//  Multiplier_FinderTests
+//
+//  Created by Brandon_Saunders on 10/8/16.
+//  Copyright © 2016 Brandon_Saunders. All rights reserved.
+//
+
+import XCTest
+@testable import Multiplier_Finder
+
+class Multiplier_FinderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

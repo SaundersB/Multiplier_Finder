f0918074225fcf586f6d964e19879d1dba8a7bb4
SaundersB <speakeasys@gmail.com>
2016-10-29 12:15:39 -0700

    Adjusting the buttons on each cell.

--- Multiplier_Finder/FlashCardCell.swift
+++ Multiplier_Finder/FlashCardCell.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class FlashCardViewCell: UICollectionViewCell {
+    var card_text: UILabel!
     var card_image_view: UIImageView!
+    var card_button: UIButton!
     
     override func awakeFromNib() {
         card_image_view = UIImageView(frame: contentView.frame)
         card_image_view.contentMode = .scaleAspectFill
         card_image_view.clipsToBounds = true
         contentView.addSubview(card_image_view)
+        
+        let x_value = contentView.frame.width - contentView.frame.width
+        let y_value = (contentView.frame.height)/2
+        let rectangle = CGRect(x: x_value, y: y_value, width: contentView.frame.width, height: 40)
+        card_button = UIButton(frame: rectangle)
+        card_button.backgroundColor = UIColor.darkGray
+        card_button.setTitle("Setting", for: .normal)
+        card_button.layer.cornerRadius = 3
+        card_button.clipsToBounds = true
+        card_button.addTarget(self, action: #selector(startPracticing), for: .touchUpInside)
+        contentView.addSubview(card_button)
+    }
+    
+    func startPracticing() {
+        print("Starting practice")
     }
 }
